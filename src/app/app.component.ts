69f60ca3d59f7be48b009b711f83f0d868f608fa
dmitriy <nikitenkovdmitriy@yandex.ru>
2019-08-05 08:49:36 +0300

    Data model

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rout',
+  template: `<div class="bg-success p-a-1 text-xs-center">
+This is SportsStore
+</div>`,
+  styleUrls: []
+})
+export class AppComponent {
+}
